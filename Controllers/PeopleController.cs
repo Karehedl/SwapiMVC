7c6b023dad246ae019c3f016c77ce87ee89d4a2a
Katelyn Hedlund <katelyn.renee1013@gmail.com>
2023-01-25 17:59:23 -0500

    ErrorsDone&PJFinsihed

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SwapiMVC.Models;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,16 +16,26 @@ namespace SwapiMVC.Controllers
 
         public async Task<IActionResult> Index(string page)
         {
-            return View();
             //route creates the endpoint we'd like to hit.
             string route = $"people?page={page ?? "1"}";
             //passes this route into our _httpClient's GetAsync method and capture the response.
-            HttpResponseMessage response = await _httpClient.GetAsync(route);
+            HttpResponseMessage response = await _httpClient.GetAsync(_httpClient.BaseAddress+route);
         
             var responseString = await response.Content.ReadAsStringAsync();
-            var people = JsonSerializer.Deserialize<ResultsViewModel<PeopleViewModel>>(responseString);
+            var people = JsonConvert.DeserializeObject<ResultsViewModel<PeopleViewModel>>(responseString);
 
             return View(people);
+        }
+
+        public async Task<IActionResult> Person(string id)
+        {
+            var response = await _httpClient.GetAsync($"people/{id}");
+                if (id is null || response.IsSuccessStatusCode == false)
+                    return RedirectToAction(nameof(Index));
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var person = JsonConvert.DeserializeObject<PeopleViewModel>(responseString);
+            return View(person);
         }
     }
 }
