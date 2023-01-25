f9778716ba66684c02f0da06e77ba8cd1ff6b578
Katelyn Hedlund <katelyn.renee1013@gmail.com>
2023-01-24 20:20:09 -0500

    PeopleControllerUpdate

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using SwapiMVC.Models;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace SwapiMVC.Controllers
 {
@@ -10,9 +13,18 @@ namespace SwapiMVC.Controllers
             _httpClient = httpClientFactory.CreateClient("swapi");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string page)
         {
             return View();
+            //route creates the endpoint we'd like to hit.
+            string route = $"people?page={page ?? "1"}";
+            //passes this route into our _httpClient's GetAsync method and capture the response.
+            HttpResponseMessage response = await _httpClient.GetAsync(route);
+        
+            var responseString = await response.Content.ReadAsStringAsync();
+            var people = JsonSerializer.Deserialize<ResultsViewModel<PeopleViewModel>>(responseString);
+
+            return View(people);
         }
     }
 }
