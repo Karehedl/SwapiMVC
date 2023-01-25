b8c700a8af6f13d9cc11d3ab5b5fe94d11572835
Katelyn Hedlund <katelyn.renee1013@gmail.com>
2023-01-24 19:47:51 -0500

    IHttpClientFactory

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -4,6 +4,12 @@ namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
+
         public IActionResult Index()
         {
             return View();
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using System.Collections.Immutable;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("swapi", client =>
+    {
+        client.BaseAddress = new Uri("https://swapi.dev/api/");
+    });
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
